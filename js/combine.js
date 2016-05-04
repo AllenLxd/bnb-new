343be6da9b6ac980b5533dc6712d564a610567a8
Allen <allenl@bnboxes.com>
2016-05-04 21:29:22 +0800

    add all

--- js/combine.js
+++ js/combine.js
@@ -4,9 +4,9 @@ $(function(){
 			   var keywords = [
                    'trident','trident sf','trident df','G2G trident',
                    'array','array SF','array DF','G2G array',
-                   'side','G2G side'
+                   'side','G2G side','wow','WOW','anpro','ANPRO','Anpro'
                   ]
-			   var videoSrc = ['Q1.mp4','Q2.mp4','Q3.mp4'];
+			   var videoSrc = ['Q1.mp4','Q2.mp4','Q3.mp4','Q4.mp4','Q5.mp4'];
 			   
 			   $('video.movie').bind('ended',function(){
 								    		 $(this).animate({
@@ -14,9 +14,27 @@ $(function(){
 												        top:'0px',
 												        width:'20px',
 												        height:'10px',									   
-													}, 1000).hide(1000);
+													}, 1000).hide(100);
 												
 										});
+				var showVideo = function(num){
+		   	
+		    	
+		           $('video.movie').animate({
+						        right:'0px',
+						        top:'0px',
+						        width:'20px',
+						        height:'10px'
+					 }, 10).hide();
+					 
+					 $('video.movie').attr({'src':videoSrc[num]}).show().animate({
+												        right:'400px',
+												        top:'200px',
+												        width:'615px',
+												        height:'350px'
+												}, 1700);
+					  
+		 }
 			   
 				 $('#search').click(function(){
 				 	    
@@ -30,37 +48,27 @@ $(function(){
 									        height:'10px'
 										}, 10).hide();
 										
-			             if(searchResult == -1){
+						 
+						//搜索判断				
+			           if(searchResult == -1){
 			             	
-			             	    $('h3').html('can not search what you want ! please contact us');
+			             	    $('p.no-search').html('can not search what you want ! please contact us');
 			             	    
 			             }else if(searchResult<=3){
-			             	      srcIndex = 0;
-			             	      $('video.movie').attr({'src':videoSrc[srcIndex]}).show().animate({
-															        right:'400px',
-															        top:'200px',
-															        width:'615px',
-															        height:'350px'
-															}, 1700);
+			             	      showVideo(0);
 			             	  
 			             }else if(searchResult<=7){
-			             	      srcIndex = 1;
-			             	      $('video.movie').attr({'src':videoSrc[srcIndex]}).show().animate({
-															        right:'400px',
-															        top:'200px',
-															        width:'620px',
-															        height:'350px'
-															}, 1700);
+			             	      showVideo(1);
 			             	  
-			             }else{
-			             	      srcIndex = 2;
-			             	      $('video.movie').attr({'src':videoSrc[srcIndex]}).show().animate({
-															        right:'400px',
-															        top:'200px',
-															        width:'620px',
-															        height:'350px'
-															}, 1700);
-			                 }
+			             }else if(searchResult<=9){
+			             	      showVideo(2);
+			             	      
+			             }else if(searchResult<=11){
+			             	      showVideo(3);
+			             	      
+			             }else if(searchResult<=14){
+			             	      showVideo(4);
+			             }
 			             
 				 });
 				 
@@ -82,10 +90,49 @@ $(function(){
 						    $('video.movie').attr({'src':videoSrc[moveIndex]}).show().animate({
 															        right:'400px',
 															        top:'200px',
-															        width:'620px',
+															        width:'615px',
 															        height:'350px'
 															}, 1700);
 					});
 				})
+				
+				
+				 if (annyang) {		  
+		    // define the functions our commands will run.			
+		 
+		    
+		
+		    // define our commands.
+		    // * The key is the phrase you want your users to say.
+		    // * The value is the action to do.
+		    //   You can pass a function, a function name (as a string), or write your function as part of the commands object.
+		    var commands = {
+		    	
+		      'trident':  function(){showVideo(0)},
+		      'array':  function(){showVideo(1)},
+			  'side':  function(){showVideo(2)} 	
+			            
+		        
+		    };
+		
+		    // OPTIONAL: activate debug mode for detailed logging in the console
+		    annyang.debug();
+		
+		    // Add voice commands to respond to
+		    annyang.addCommands(commands);
+		
+		    // OPTIONAL: Set a language for speech recognition (defaults to English)
+		    // For a full list of language codes, see the documentation:
+		    // https://github.com/TalAter/annyang/blob/master/docs/README.md#languages
+		    annyang.setLanguage('en');
+		
+		    // Start listening. You can call this here, or attach this call to an event, button, etc.
+		    annyang.start();
+		    
+		  } else {
+		    $(document).ready(function() {
+		      console.log('can not support')
+		    });
+		  }
 			    	
      })
